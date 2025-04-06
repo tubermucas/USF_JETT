1856010847e1ee90afd727a4b1bf6fbc6943ecf4
tubermucas <tubermucas@mail.ru>
2025-04-05 20:18:30 -0400

    Add camera footages and user input

--- USF_JETT.py
+++ USF_JETT.py
@@ -1,6 +1,9 @@
 from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
 from msrest.authentication import ApiKeyCredentials
 
+import os
+import random
+
 ENDPOINT = "https://tableemptypredictor.cognitiveservices.azure.com/"
 PREDICTION_KEY = "ExYj2VC82WJsTbgsB5JcdfvAJArTjTXtw01JxyYfV9iUgVTAe2qdJQQJ99BDACYeBjFXJ3w3AAAIACOGOjam"
 PROJECT_ID = "38320eb8-3bf2-43e2-8766-00fda8ed5144"
@@ -37,8 +40,10 @@ def is_overlap(box1, box2):
 
 def count_occupied_tables(predictions):
     print("count_occupied_tables func is running")
-    people = [p for p in predictions if p.tag_name == "Person" and p.probability > 0.5]
-    tables = [t for t in predictions if t.tag_name == "Table" and t.probability > 0.5]
+    people = [p for p in predictions if p.tag_name == "Person" and p.probability > 0.7]
+    print(f"people: {len(people)}")
+    tables = [t for t in predictions if t.tag_name == "table" and t.probability > 0.7]
+    print(f"tables: {len(tables)}")
 
     occupied = 0
     for table in tables:
@@ -50,14 +55,52 @@ def count_occupied_tables(predictions):
     total_tables = len(tables)
     return occupied, total_tables
 
+def get_random_camera_footage(building_name):
+
+    random_footage_path = os.path.join(f"camera_footages/{building_name}", random.choice(os.listdir(f"camera_footages/{building_name}")))
+    if not os.path.exists(random_footage_path):
+        print(f"Error: The file {random_footage_path} does not exist.")
+        return None
+    return random_footage_path
+
 def main():
-    # Example usage
-    image_path = "LIB_medium.jpg"  # Replace with your image path
+    """
+    available_buildings = ["LIB", "BSN", "ENB", "MDN"]
+    
+    building_choice = input("Enter the building name: ")
+
+    cur_photo = building_choice.upper()
+    print(f"Current photo: {cur_photo}")
+
+    if building_choice not in available_buildings:
+        print(f"Invalid building name.")
+        return
+
+    image_path = get_random_camera_footage(building_choice)
+    if not image_path:
+        return
+    
+    cur_photo = image_path.upper()
+    print(f"Current photo: {cur_photo}")
+    
+    """
+
+    image_path = "camera_footages/MDN/MDN_full_ai.png"
+    
+
     predictions = detect_objects(image_path)
     occupied, total = count_occupied_tables(predictions)
     print(f"{occupied} out of {total} tables are occupied.")
 
     precent_of_occupied = (occupied / total) * 100 if total > 0 else 0
     print(f"Percentage of occupied tables: {precent_of_occupied:.2f}%")
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
 
 
