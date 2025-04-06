1e34e521a9e2aac95e4f42e502c4a7f9b17bb1c1
jessesim <jessesimmons12@yahoo.com>
2025-04-06 08:49:34 -0400

    conflict res

--- app/page.js
+++ app/page.js
@@ -74,8 +74,7 @@ function USFJETT() {
     <div className="flex flex-col items-center p-6 space-y-6">
       <h1 className="text-3xl font-bold text-green-600">USF JETT</h1>
       <p className="text-lg text-gray-600">
-        The USF JETT app tells you currennt occupancy in these specific study areas
-        and predicts future occupancy based on historical data.
+      Discover real-time study space occupancy, upload your own images, integrate your schedule to see the best place to study in between classes, and book a  private study space with the USF Jett App!.
       </p>
     
 
